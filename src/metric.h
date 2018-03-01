a102bff10b3afb5d732b675046195dc9a17daf3d
“刘达” <imliuda@163.com>
2018-03-02 00:17:13 +0800

    metric interface defination

--- src/metric.h
+++ src/metric.h
@@ -5,16 +5,57 @@
 
 #include "map.h"
 
+#define EQ_NAME     1
+#define EQ_TAGS     2
+#define EQ_FIELDS   4
+#define EQ_TIME     8
+
+enum metric_vtype { FLOAT, INTEGER, STRING, BOOLEAN};
+enum metric_precision {NSEC, USEC, MSEC, SECOND, MINUTE, HOUR};
+
+struct metric_value {
+    enum metric_vtype type;
+    union {
+        double fv;
+        int64_t iv;
+        char *sv;
+        bool bv;
+    } value;
+}
+
+struct metric_time {
+    struct timespec time;
+    enum precision precision;
+}
+
 struct metric {
     char *name;
     struct map *tags;
     struct map *fields;
-    struct timespec time;
+    struct metric_time time;
 };
 
 // clock_gettime(clockid_t clk_id, struct timespec *tp);
 // CLOCK_REALTIME
 
-void metric_parse(const char *buf, struct list **metrics);
+struct metric *metric_new(const char *name);
+void metric_new_full(const char *name, struct map *tags, struct map *fields, struct metric_time time);
+struct list *metric_parse(const char *buf);
+char *metric_serialize(struct list *metrics);
+bool metric_equal(struct metric *metric1, struct metric *metric2, int mode);
+char *metric_get_name(struct metric *metric);
+void metric_set_name(struct metric *metric, char *name);
+struct list * metric_tags(struct metric *metric);
+void metric_add_tag(struct metric *metric, const char *tag, const char *value);
+char *metric_get_tag(struct metric *metric, const char *tag);
+bool metric_update_tag(struct metric *metric, const char *tag, const char *value);
+bool metric_remove_tag(struct metric *metric, const char *tag);
+struct list *metric_fields(struct metric *metric);
+void metric_add_field(struct metric *metric, const char *field, struct metric_value *value);
+struct metric_value *metric_get_field(struct metric *metric, const char *field);
+bool metric_update_field(struct metric *metric, const char *field, struct metric_value *value);
+bool metric_remove_field(struct metric *metric, const char *field);
+struct metric_time metric_get_time(struct metric *metric);
+void metric_set_time(struct metric *metric, struct metric_time time);
 
 #endif
