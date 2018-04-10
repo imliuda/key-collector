e87da2ec268aac5cc88505c83b55c7a9767ef0e3
“刘达” <imliuda@163.com>
2018-04-11 02:36:58 +0800

    update metric defination

--- src/metric.h
+++ src/metric.h
@@ -3,53 +3,33 @@
 
 #include <time.h>
 
-#include "map.h"
+#include "json.h"
 
 #define METRIC_EQUAL_NAME     1
 #define METRIC_EQUAL_TAGS     2
-#define METRIC_EQUAL_FIELDS   4
+#define METRIC_EQUAL_VALUE    4
 #define METRIC_EQUAL_TIME     8
 
-enum field_value_type { 
-    FIELD_STRING_TYPE,
-    DIELD_INTEGER_TYPE,
-    FIELD_FLOAT_TYPE,
-    FIELD_BOOLEAN_TYPE
-};
-
-struct field_value {
-    enum field_value_type type;
-    union {
-        double dbl_v;
-        int64_t int_v;
-        char *str_v;
-        bool bool_v;
-    } value;
-};
-
 struct metric {
-    char *name;
-    struct map *tags;
-    struct map *fields;
-    time_t time;
+    const char *name;
+    struct json *tags;
+    struct json *value;
+    struct timespec time;
 };
 
 struct metric *metric_new();
-void metric_destroy(struct metric *metric);
+void metric_destroy(struct metric *m);
 struct list *metric_parse(const char *buf);
-const char *metric_serialize(struct list *metrics);
-bool metric_equal(struct metric *metric1, struct metric *metric2, int mode);
-const char *metric_get_name(struct metric *metric);
-void metric_set_name(struct metric *metric, char *name);
-struct list * metric_tag_keys(struct metric *metric);
-void metric_add_tag(struct metric *metric, const char *tag, const char *value);
-char *metric_get_tag(struct metric *metric, const char *tag);
-bool metric_remove_tag(struct metric *metric, const char *tag);
-struct list *metric_field_keys(struct metric *metric);
-void metric_add_field(struct metric *metric, const char *field, struct field_value *value);
-struct field_value *metric_get_field(struct metric *metric, const char *field);
-bool metric_remove_field(struct metric *metric, const char *key);
-time_t metric_get_time(struct metric *metric);
-void metric_set_time(struct metric *metric, time_t time);
+const char *metric_serialize(struct metric *m);
+bool metric_equal(struct metric *m1, struct metric *m2, int mode);
+void metric_set_name(struct metric *m, const char *name);
+const char *metric_get_name(struct metric *m);
+struct list *metric_tag_keys(struct metric *m);
+void metric_add_tag(struct metric *m, const char *key, const char *value);
+const char *metric_get_tag(struct metric *m, const char *key);
+void metric_set_value(struct metric *m, struct json *value);
+struct json *metric_get_value(struct metric *m);
+void metric_set_time(struct metric *m, struct timespec time);
+struct timespec metric_get_time(struct metric *m);
 
 #endif
