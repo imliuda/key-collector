c2421eb9c9003b859050016bb5d4dbf93661027d
刘达 <liuda@yun-ji.cn>
2018-05-07 14:18:10 +0800

    modify metric interface

--- src/metric.h
+++ src/metric.h
@@ -2,34 +2,62 @@
 #define __OSCLT_METRIC_H__
 
 #include <time.h>
-#include <wchar.h>
 
-#include "json.h"
+#include "map.h"
+#include "list.h"
 
 #define METRIC_EQUAL_NAME     1
 #define METRIC_EQUAL_TAGS     2
 #define METRIC_EQUAL_VALUE    4
 #define METRIC_EQUAL_TIME     8
 
+enum metric_value_type {
+    METRIC_VALUE_STRING,
+    METRIC_VALUE_INTEGER,
+    METRIC_VALUE_REAL,
+    METRIC_VALUE_BOOLEAN,
+    METRIC_VALUE_NULL
+};
+
+struct metric_value {
+    enum metric_value_type type;
+    union data {
+        char *strv;
+        long long intv;
+        double realv;
+        bool boolv;
+    };
+};
+
 struct metric {
-    struct json *data;
+    char *name;
+    struct map *tags
+    struct metric_value value;
+    time_t time;
 };
 
 struct metric *metric_new();
 void metric_destroy(struct metric *m);
-void metric_list_destroy(struct list *ms);
-struct list *metric_loads(const char *buf);
-char *metric_dumps(struct metric *m);
-char *metric_list_dumps(struct list *ms);
+struct list *metric_parse(const char *buf);
+char *metric_serialize(struct metric *m);
+char *metric_list_serialize(struct list *l);
 bool metric_equal(struct metric *m1, struct metric *m2, int mode);
 void metric_set_name(struct metric *m, const char *name);
 const char *metric_get_name(struct metric *m);
 struct list *metric_tag_keys(struct metric *m);
-void metric_add_tag(struct metric *m, const char *key, const char *value);
-const char *metric_get_tag(struct metric *m, const char *key);
-void metric_set_value(struct metric *m, struct json *value);
-struct json *metric_get_value(struct metric *m);
-void metric_set_time(struct metric *m, time_t time);
+void metric_add_tag(struct metric *m, const char *k, const char *v);
+const char *metric_get_tag(struct metric *m, const char *k);
+enum metric_value_type metric_value_type(struct metric_value *v);
+void metric_set_string_value(struct metric *m, const char *v);
+void metric_set_integer_value(struct metric *m, long long v);
+void metric_set_real_value(struct metric *m, double v);
+void metric_set_boolean_value(struct metric *m, bool v);
+void metric_set_null_value(struct metric *m);
+const char *metric_get_string_value(struct metric *m);
+long long metric_get_integer_value(struct metric *m);
+double  metric_get_real_value(struct metric *m);
+bool metric_get_boolean_value(struct metric *m);
+void metric_set_time(struct metric *m, time_t t);
 time_t metric_get_time(struct metric *m);
 
 #endif
