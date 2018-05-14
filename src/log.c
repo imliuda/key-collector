ce387ffc15acf8a6ad4efe51074fb3ac2d830bea
刘达 <liuda@yun-ji.cn>
2018-05-14 17:50:22 +0800

    update log

--- src/log.c
+++ src/log.c
@@ -12,9 +12,18 @@
 #define _GNU_SOURCE
 
 static struct queue *log_queue;
+static size_t log_queue_size;
 static const char *log_file;
 static enum log_level log_level;
-ev_io log_watcher;
+static struct ev_loop *loop;
+static ev_io log_watcher;
+static ev_timer retry_watcher;
+
+static void retry_write_log(EV_P_ ev_timer *w, int revents) {
+    if (revents & EV_TIMER) {
+        ev_feed_event(loop, &log_watcher, EV_WRITE);
+    }
+}
 
 static void write_log(EV_P_ ev_io *w, int revents) {
     if (revents & EV_WRITE) {
@@ -22,15 +31,19 @@ static void write_log(EV_P_ ev_io *w, int revents) {
         while(e = queue_front(log_queue)) {
             size_t nwrite = write(w->fd, e->text + e->nwrite, e->length - e->nwrite);
             if (nwrite == -1) {
-                /* try it next time */
                 break;
             } else {
                 e->nwrite += nwrite;
             }
             if (e->nwrite == e->length) {
-                e = queue_pop(log_queue);
-                free(e->text);
-                free(e);
+                if (fsync(w->fd) != -1) {
+                    e = queue_pop(log_queue);
+                    free(e->text);
+                    free(e);
+                } else {
+                    e->nwrite = 0;
+                    break;
+                }
             }
         }
     }
@@ -39,6 +52,12 @@ static void write_log(EV_P_ ev_io *w, int revents) {
 void LOG(enum log_level level, const char *fmt, va_list args) {
     if (level < log_level)
         return;
+
+    if (queue_size(log_queue) >= log_queue_size) {
+        /* just drop the data. later, may add self-monitoring static metric. */
+        return;
+    }
+
     struct log_entry *entry = malloc(sizeof(struct log_entry));
     entry->level = level;
 
@@ -47,7 +66,6 @@ void LOG(enum log_level level, const char *fmt, va_list args) {
     struct tm *tm = gmtime(&t);
     size_t tlen = strftime(time_buf, 63, "%Y-%m-%d %H:%M:%S", tm);
     time_buf[tlen] = '\0';
-    //free(tm);
 
     struct strbuf *sb = strbufnew(512);
     strbufexts(sb, time_buf); 
@@ -75,9 +93,13 @@ void LOG(enum log_level level, const char *fmt, va_list args) {
     entry->length = strlen(text);
 
     queue_push(log_queue, entry);
+
+    ev_feed_event(loop, &log_watcher, EV_WRITE);
 }
 
 void log_init(struct osclt *oc) {
+    loop = oc->loop;
+ 
     const char *level = config_get_string(oc->config, "log_level", "warn");
 
     if (strcmp(level, "debug") == 0) {
@@ -96,8 +118,9 @@ void log_init(struct osclt *oc) {
     }
 
     log_file = config_get_string(oc->config, "log_file", "/var/log/osclt/osclt.log");
+    log_queue_size = config_get_integer(oc->config, "log_queue_size", 1000);
 
-    int fd = open(log_file, O_WRONLY | O_CREAT);
+    int fd = open(log_file, O_RDWR | O_APPEND | O_CREAT, 0755);
 
     if (fd == -1) {
         fprintf(stderr, "open log file failed: \"%s\".", log_file);
@@ -107,8 +130,14 @@ void log_init(struct osclt *oc) {
     int flags = fcntl(fd, F_GETFL, 0);
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 
-    ev_io_init(&log_watcher, write_log, fd, EV_WRITE);
-    ev_io_start(oc->loop, &log_watcher);
+    /* write log queue to file every 1 second, used for handling
+       unsuccessful log write */
+    ev_timer_init(&retry_watcher, retry_write_log, 0, 1);
+    ev_timer_start(loop, &retry_watcher);
+
+    /* new log comes, write it right now with ev_feed_event(). */
+    ev_io_init(&log_watcher, write_log, fd, EV_NONE);
+    ev_io_start(loop, &log_watcher);
 
     log_queue = queue_new();
 }
